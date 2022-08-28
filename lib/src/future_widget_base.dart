c2f967591d8926b5f21b1123e8083ab902b466b5
Kar1mMohamed <karimo741852@gmail.com>
2022-08-28 10:45:22 +0200

    .

--- lib/src/future_widget_base.dart
+++ lib/src/future_widget_base.dart
@@ -1,13 +1,22 @@
 import 'package:get_server/get_server.dart'
     show SenderWidget, Widget, BuildContext, WidgetEmpty;
 
-class FutureWidget extends SenderWidget {
-  FutureWidget(this.future);
-  final Future<String> future;
+class FutureWidget extends _BaseFuturerWidget {
+  FutureWidget(Future<String> future) : super(future);
 
   @override
   Widget build(BuildContext context) {
     if (context.request.method == 'OPTIONS') return WidgetEmpty();
+    return _BaseFuturerWidget(future);
+  }
+}
+
+class _BaseFuturerWidget extends SenderWidget {
+  _BaseFuturerWidget(this.future);
+  final Future<String> future;
+
+  @override
+  Widget build(BuildContext context) {
     future.then(
       (value) {
         context.request.response!.send(value);
